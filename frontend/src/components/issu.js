518859a263ea80995f3e5f3be2aaad1aa160fd7a
SalvadorScafati <seviperclash@gmail.com>
2024-09-14 12:02:26 -0300

    feat: add 38

--- frontend/src/components/issu.js
+++ frontend/src/components/issu.js
@@ -8,7 +8,7 @@ const Issu = () => {
         sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms"
         allowFullScreen={true}
         style={{ position: 'absolute', border: 'none', width: '100%', height: '100%', left: 0, right: 0, top: 0, bottom: 0 }}
-        src="https://e.issuu.com/embed.html?d=bds_final_37&u=bocadesapo"
+        src="https://e.issuu.com/embed.html?d=bds_38&u=bocadesapo"
         title="Embedded Issuu Document"
       />
     </div>
